3ff7e1e5101c0a334bba9cb963e3196dd2dee555
Katka <kuberova.kata@gmail.cz>
2026-02-05 16:35:40 +0100

    command ukoncit

---
+++ src/Command/Ukoncit.java
@@ -0,0 +1,27 @@
+package Command;
+
+import Hra.StavHry;
+
+public class Ukoncit implements Command{
+
+    @Override
+    public String execute(StavHry stavHry) {
+        return "";
+
+    }
+
+    public boolean exit(StavHry stavHry) {
+        if (stavHry.zkontrolujKonecMise()) {
+            System.out.println("--------------------------------------------------\n" +
+                    "SYSTÉM: Všechny úkoly splněny. Data odeslána na Zemi.\n" +
+                    "Nix usedá do křesla, motory modulu naskakují...\n" +
+                    "Mise AETHEREA byla úspěšně dokončena! Vyhrál jsi!\n" +
+                    "--------------------------------------------------");
+            return true;
+        } else {
+            System.out.println("NIX: Ještě nemůžu odletět. Musím dokončit všechny úkoly " +
+                    "a prozkoumat celou stanici na 100 %.");
+            return false;
+        }
+    }
+}
